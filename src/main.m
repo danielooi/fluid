09b700b05e13a591c49b04f853b6960ee401aeea
Todd <todd.ditchendorf@gmail.com>
2010-05-07 21:11:38 -0700

    updating lic, ignores

--- src/main.m
+++ src/main.m
@@ -1,9 +1,23 @@
+//  Copyright 2009 Todd Ditchendorf
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 //
 //  main.m
 //  Fluid
 //
-//  Created by Todd Ditchendorf on 11/23/07.
-//  Copyright __MyCompanyName__ 2007. All rights reserved.
+//  Created by Todd Ditchendorf on 11/23/07. <-- the day Fluid was born
+//  Copyright Todd Ditchendorf 2007. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
